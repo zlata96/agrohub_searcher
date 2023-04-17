a3b13a188054454a9333cffcb3ee1807c2cf43e3
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-17 23:58:19 +0400

    Add UIColor Extension

---
+++ Agrohub_searcher/Helpers/Extensions/UIColor+Extension.swift
@@ -0,0 +1,11 @@
+// UIColor+Extension.swift
+// AgroHub_Searcher. Created by Zlata Guseva.
+
+import UIKit
+
+extension UIColor {
+    static let subTitleTextColor = UIColor(named: "subTitleTextColor")
+    static let mainTitleTextColor = UIColor(named: "mainTitleTextColor")
+    static let mainBackground = UIColor(named: "mainBackground")
+    static let navigationBackground = UIColor(named: "navigationBackground")
+}
