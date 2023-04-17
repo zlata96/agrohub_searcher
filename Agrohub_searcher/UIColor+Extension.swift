780716ad2aa832c6bd53e5f1d1bb71bc0d29a9be
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-17 15:36:45 +0400

    Add new color sets

---
+++ Agrohub_searcher/UIColor+Extension.swift
@@ -0,0 +1,10 @@
+// UIColor+Extension.swift
+// AgroHub_Searcher. Created by Zlata Guseva.
+
+import UIKit
+
+extension UIColor {
+    static let subTitleTextColor = "subTitleTextColor"
+    static let mainTitleTextColor = "mainTitleTextColor"
+    static let mainBackground = "mainBackground"
+}
