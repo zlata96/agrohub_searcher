2e81abb8330462c4de4ca8f99c65e1d7a6c5ac5f
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-17 14:36:28 +0400

    Initial commit

---
+++ Agrohub_searcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Agrohub_searcher
+//
+//  Created by Злата Гусева on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
