d77cfca0f647d92b518f6373105fbc89e7e72080
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-18 17:47:48 +0400

    Add itemViewController

--- Agrohub_searcher/ProductItem/ItemView.swift
+++ Agrohub_searcher/ProductItem/ItemView.swift
@@ -4,10 +4,127 @@
 import UIKit
 
 class ItemView: UIView {
-    var itemImageView = UIImageView()
-    var starButton = UIButton()
-    var itemLabel = UILabel()
-    var iteDescriptionLabel = UILabel()
-    var findLocationButton = UIButton()
-    var additionalImageView = UIImageView()
+    private var itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private var starButton: UIButton = {
+        var button = UIButton()
+        button.setImage(UIImage(named: "starIcon"), for: .normal)
+        button.contentMode = .center
+        button.tintColor = .subTitleTextColor
+        return button
+    }()
+
+    private var itemLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .mainTitleTextColor
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private var itemDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = .subTitleTextColor
+        label.numberOfLines = 0
+        return label
+    }()
+
+    private var findLocationButton: UIButton = {
+        var button = UIButton()
+        button.setImage(UIImage(named: "locationIcon"), for: .normal)
+        button.setTitle(" ГДЕ КУПИТЬ ", for: .normal)
+        button.setTitleColor(.mainTitleTextColor, for: .normal)
+        button.tintColor = .navigationBackground
+        button.contentMode = .center
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.subTitleTextColor?.cgColor
+        button.layer.cornerRadius = 4
+        return button
+    }()
+
+    private var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func commonInit() {
+        setupStyle()
+        addSubviews()
+        makeConstraints()
+    }
+
+    private func setupStyle() {
+        backgroundColor = .mainBackground
+    }
+
+    private func addSubviews() {
+        addSubview(itemImageView)
+        addSubview(starButton)
+        addSubview(itemLabel)
+        addSubview(itemDescriptionLabel)
+        addSubview(findLocationButton)
+        addSubview(iconImageView)
+    }
+
+    private func makeConstraints() {
+        itemImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(40)
+            make.height.equalTo(183)
+            make.width.equalTo(117)
+        }
+
+        starButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(36)
+            make.top.equalToSuperview().offset(40)
+            make.size.equalTo(28)
+        }
+
+        iconImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(24)
+            make.top.equalToSuperview().offset(40)
+            make.size.equalTo(30)
+        }
+
+        itemLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(14)
+            make.top.equalTo(itemImageView.snp.bottom).offset(32)
+        }
+
+        itemDescriptionLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(14)
+            make.top.equalTo(itemLabel.snp.bottom).offset(8)
+        }
+
+        findLocationButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(14)
+            make.top.equalTo(itemDescriptionLabel.snp.bottom).offset(16)
+            make.height.equalTo(26)
+        }
+    }
+}
+
+extension ItemView: Configurable {
+    func configure(with model: Product) {
+        let imageURL = "http://shans.d2.i-partner.ru" + model.image
+        itemImageView.kf.setImage(with: URL(string: imageURL.preparedStringForURL))
+        itemLabel.text = model.name
+        itemDescriptionLabel.text = model.description
+    }
 }
--- Agrohub_searcher/ProductItem/ItemViewController.swift
+++ Agrohub_searcher/ProductItem/ItemViewController.swift
@@ -3,11 +3,27 @@
 
 import UIKit
 
-class ItemViewController: UINavigationController {
+class ItemViewController: NavigationController {
     var itemView = ItemView()
+    var product: Product
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+
+    init(product: Product) {
+        self.product = product
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupView() {
         view = itemView
+        itemView.configure(with: product)
     }
 }
