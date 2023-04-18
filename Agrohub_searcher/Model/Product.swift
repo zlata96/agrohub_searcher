d153eb70c5ed87644abfec8dc30901c8e9f9ca57
Zlata Guseva <gusevazlata96@gmail.com>
2023-04-18 17:46:43 +0400

    Add Data model

---
+++ Agrohub_searcher/Model/Product.swift
@@ -0,0 +1,10 @@
+// Product.swift
+// AgroHub_Searcher. Created by Zlata Guseva.
+
+import Foundation
+
+struct Product: Codable {
+    let id: Int
+    let image: String
+    let name, description: String
+}
